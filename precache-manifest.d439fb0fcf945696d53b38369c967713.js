31541cbc6002a736fc82f7c8ca146fbff12df04d
Liliia <chuiko1112@gmail.com>
2020-06-24 11:39:41 +0300

    Updates

---
+++ precache-manifest.d439fb0fcf945696d53b38369c967713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5419a47856b9bce00efcd326dc70f1f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6178230338af26a533f9",
+    "url": "./static/css/main.8895ac16.chunk.css"
+  },
+  {
+    "revision": "005c9d4e6d890a5c310b",
+    "url": "./static/js/2.10d02390.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.10d02390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6178230338af26a533f9",
+    "url": "./static/js/main.408245ee.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
